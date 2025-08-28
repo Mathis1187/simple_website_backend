a728bd3671f735e5829a6baaf1ec0e5d94ce5a0f
yanis26x <yanis.djenadi@hotmail.com>
2025-08-28 10:28:35 -0400

    Controller fait

--- src/main/java/mathis/simple_website_backend/controller/PeopleController.java
+++ src/main/java/mathis/simple_website_backend/controller/PeopleController.java
@@ -1,5 +1,24 @@
 package mathis.simple_website_backend.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mathis.simple_website_backend.models.People;
+import mathis.simple_website_backend.services.PeopleService;
+
+
+import java.util.List;
+@RestController
+@RequestMapping("/people")
 
 public class PeopleController {
+    @Autowired
+    private PeopleService peopleService;
+
+    @GetMapping("/PeopleService")
+    public List<People> getAllPeople() {
+        return peopleService.getAllPeople();
+    }
 
 }
