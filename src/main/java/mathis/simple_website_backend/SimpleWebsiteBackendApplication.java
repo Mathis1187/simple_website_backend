288e236361e6d5b78b6b38b811d85d736bae3031
Mathis1187 <mathisvincent1@gmail.com>
2025-08-28 09:03:26 -0400

    test

---
+++ src/main/java/mathis/simple_website_backend/SimpleWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package mathis.simple_website_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebsiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mathis/simple_website_backend/SimpleWebsiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mathis.simple_website_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebsiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
