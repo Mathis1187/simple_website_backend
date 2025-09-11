ea579ed332885a87e8aa82fb0805641f2593f7ad
yanis26x <yanis.djenadi@hotmail.com>
2025-09-11 09:00:28 -0400

    model Series fait

---
+++ src/main/java/mathis/simple_website_backend/models/Series.java
@@ -0,0 +1,55 @@
+package mathis.simple_website_backend.models;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Series {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String titre;
+    private String genre;
+    private int nbEpisodes;
+    private float note;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getNbEpisodes() {
+        return nbEpisodes;
+    }
+
+    public void setNbEpisodes(int nbEpisodes) {
+        this.nbEpisodes = nbEpisodes;
+    }
+
+    public float getNote() {
+        return note;
+    }
+
+    public void setNote(float note) {
+        this.note = note;
+    }
+}
